a791a342b82e6fc5114d5b0d14aa9e024473212c
Alvaro Alves <alvaro.oliveira@seplan.pi.gov.br>
2025-07-04 13:19:29 -0300

    alteracao para marcar setor atual

--- app/src/components/select/setor/index.tsx
+++ app/src/components/select/setor/index.tsx
@@ -1,16 +1,16 @@
 import { Setor } from "@/models/Auth"
 
 
-export default async function SelectSetor() {
+export default async function SelectSetor({ id_setor_atual }: {id_setor_atual?: string}) {
     
     const setores: Setor[] = await fetch(`http://localhost:8000/setores.json`)
         .then(res => res.json())
         .then(data => data.results)
-
+    console.log(id_setor_atual)
     return (
-        <select name="setor" id="setor" className="border border-sky-600 rounded-sm">
+        <select name="setor" id="setor" className="border border-sky-600 rounded-sm" defaultValue={id_setor_atual}>
             {setores.map((setor: Setor) => (
-                <option key={setor.id.toString()} defaultValue={setor.id.toString()}>{setor.nome}</option>
+                <option key={setor.id.toString()} value={setor.id.toString()}>{setor.nome}</option>
             ))}
         </select>
     )
