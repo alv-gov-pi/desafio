85c892a633ee3137c8c556a6b11d2fff5e59672c
Alvaro Alves <alvaro.oliveira@seplan.pi.gov.br>
2025-07-09 13:12:15 -0300

    correção do import do mantine

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ColorSchemeScript, MantineProvider, mantineHtmlProps } from '@mantine/core';
 import NextAuthSessionProvider from "@/providers/session-provider";
-
+import MantineWrapper from "@/components/template/autenticado";
 export const metadata: Metadata = {
   title: "Atendimentos seplan",
   description: "Desafio",
@@ -22,7 +22,7 @@ export default function RootLayout({
       <body>
 
         <NextAuthSessionProvider>
-          <MantineProvider>{children}</MantineProvider>
+          <MantineWrapper>{children}</MantineWrapper>
         </NextAuthSessionProvider>
 
       </body>
