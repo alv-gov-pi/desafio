54a293cf80c44a72d0c96d2add67e7ecbe41fc9b
Alvaro Alves <alvaro.oliveira@seplan.pi.gov.br>
2025-07-10 11:52:22 -0300

    mudando de grid para flex

--- app/src/components/forms/usuario/cadastro/index.tsx
+++ app/src/components/forms/usuario/cadastro/index.tsx
@@ -14,31 +14,31 @@ function FormCadastroUsuario() {
             }
         }
     return (
-    <form action={signup} className='grid content-center gap-2 w-150'>
-        <div className="grid">
+    <form action={signup} className='flex flex-col gap-2 w-4/12 h-4/12'>
+        <div className="flex flex-col">
           <label htmlFor="nome"/>Nome Completo
           <input type="text" id='nome' className="border border-sky-600 rounded-sm" name="nome"/>
         </div>
-        <div className="grid">
+        <div className="flex flex-col">
           <label htmlFor="email"/>Email
           <input type="text" id='email' className="border border-sky-600 rounded-sm" name="email"/>
         </div>
-        <div className="grid">
+        <div className="flex flex-col">
           <label htmlFor="genero"/>Genero
           <select name="genero" id="genero" className="border border-sky-600 rounded-sm">
             <option value="F">Feminino</option>
             <option value="M">Masculino</option>
           </select>
         </div>
-        <div className="grid">
+        <div className="flex flex-col">
               <label htmlFor="password"/>Senha
               <input type="password" id='password' className="border border-sky-600 rounded-sm" name="password"/>
         </div>
-        <div className="grid">
+        <div className="flex flex-col">
           <label htmlFor="setor"/>Setor
           <SelectSetor />
         </div>
-        <div className="grid grid-cols-2 content-between">
+        <div className="flex content-between">
             <div><button className="bg-green-600 p-2 rounded-sm w-24 text-white" type="submit">Salvar</button></div>
         </div>
     </form>
