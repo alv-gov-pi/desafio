77fe6446be11272e22bc5e0d86819664ab304ec3
Alvaro Alves <alvaro.oliveira@seplan.pi.gov.br>
2025-07-10 11:13:57 -0300

    correção de importação

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ColorSchemeScript, MantineProvider, mantineHtmlProps } from '@mantine/core';
 import NextAuthSessionProvider from "@/providers/session-provider";
-import MantineWrapper from "@/components/template/autenticado";
+import MantineWrapper from "@/components/template/autenticado/MantineWrapper";
 export const metadata: Metadata = {
   title: "Atendimentos seplan",
   description: "Desafio",
