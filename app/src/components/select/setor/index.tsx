d84cd63f92a6e04340cabad5928ab4399ca7c1d8
Alvaro Alves <alvaro.oliveira@seplan.pi.gov.br>
2025-07-15 13:28:34 -0300

    mudança na importação e remoção de mensagem de debug

--- app/src/components/select/setor/index.tsx
+++ app/src/components/select/setor/index.tsx
@@ -1,12 +1,10 @@
-import { Setor } from "@/models/Auth"
-
+import { Setor } from "@/types/setor"
 
 export default async function SelectSetor({ id_setor_atual }: {id_setor_atual?: string}) {
     
     const setores: Setor[] = await fetch(`http://localhost:8000/setores.json`)
         .then(res => res.json())
-        .then(data => data.results)
-    console.log(id_setor_atual)
+  
     return (
         <select name="setor" id="setor" className="border border-sky-600 rounded-sm" defaultValue={id_setor_atual}>
             {setores.map((setor: Setor) => (
