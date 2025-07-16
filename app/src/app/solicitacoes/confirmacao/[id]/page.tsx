41ac500fc6984e0439a7f8e728702bd969d3ce53
Alvaro Alves <alvaro.oliveira@seplan.pi.gov.br>
2025-07-16 13:43:55 -0300

    remoção de imoportação não usada, uso de componente FormSolicitacaoServico

--- app/src/app/solicitacoes/confirmacao/[id]/page.tsx
+++ app/src/app/solicitacoes/confirmacao/[id]/page.tsx
@@ -1,14 +1,15 @@
+import { NumberInput, TextInput, Button } from '@mantine/core';
 import TemplateApp from "@/components/template/autenticado/template";
 import { Servico } from "@/types/servico";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { getServerSession } from "next-auth";
 import { Usuario } from "@/types/usuario";
-import { Atendimento } from "@/types/atendimento";
 
+import FormSolicitacaoServico from '@/components/forms/servico/solicitacao';
 export default async function confirmacao({ params }: { params: Promise<{ id: number }> }) {
     const { id } = await params;
     const session = await getServerSession(authOptions)
-    
+
     const emailUsuario = session?.user?.email;
     const response = await fetch(`http://localhost:8000/servico/${id}`, {
         method: "GET"
@@ -18,21 +19,13 @@ export default async function confirmacao({ params }: { params: Promise<{ id: nu
     const responseSolicitante = await fetch(`http://localhost:8000/usuario/por-email/${emailUsuario}/`, {
         method: "GET"
     })
-    const solicitante : Usuario = await responseSolicitante.json();
-    console.log(solicitante.nome);
-    const atendimentoFormada = new FormData;
-    atendimentoFormada.append('servico', `${servico.id}`)
-    atendimentoFormada.append('solicitante', `${solicitante.id}`)
-    const responseAtendimento = await fetch(`http://localhost:8000/atendimentos/`, {
-        method: "POST",
-        body: atendimentoFormada
-    })
-
-    const atendimento: Atendimento = await responseAtendimento.json();
-  
+    const solicitante: Usuario = await responseSolicitante.json();
+   
+    const props = {servico, solicitante}
+   
     return (
         <TemplateApp>
-            <div><h1>Você solicitou o serviço {servico.nome}, foi registrado com identificador {`${atendimento.id}`}, a equipe da unidade em breve começara a atuar no seu caso.</h1></div>
+            <FormSolicitacaoServico props={props}></FormSolicitacaoServico>
         </TemplateApp>
     );
 }
