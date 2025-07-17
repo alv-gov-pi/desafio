1ec2dee6e9c4b04a63ce8d82fa51f5c519b152c7
Alvaro Alves <alvaro.oliveira@seplan.pi.gov.br>
2025-07-17 13:15:41 -0300

    adição dos atributos _detalhado

--- app/src/types/atendimento.ts
+++ app/src/types/atendimento.ts
@@ -4,8 +4,11 @@ import { Usuario } from "./usuario"
 export type Atendimento = {
     id: number,
     servico: Servico,
+    servico_detalhado: Servico,
     solicitante: Usuario,
+    solicitante_detalhado: Usuario,
     responsavel: Usuario,
+    responsavel_detalhado: Usuario,
     atendido: boolean,
     cadastrado_em: Date,
     observacao: string,
