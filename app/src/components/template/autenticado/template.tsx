b2a8de9242eefe38ba836b619b46d031368d4b73
Alvaro Alves <alvaro.oliveira@seplan.pi.gov.br>
2025-07-17 13:43:06 -0300

    remoção de códifo de teste

--- app/src/components/template/autenticado/template.tsx
+++ app/src/components/template/autenticado/template.tsx
@@ -1,9 +1,12 @@
 import HeaderApp from "./header/header";
 import FooterApp from "./footer";
-export default function TemplateApp({ children }: { children: React.ReactNode }) {
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { getServerSession } from "next-auth";
+export  default async function TemplateApp({ children }: { children: React.ReactNode }) {
+    const  session = await getServerSession(authOptions)
     return (
         <div className="flex flex-col h-screen justify-between">
-            <HeaderApp usuario_id={7} />
+            <HeaderApp usuario_id={session?.user.id} />
             <main className="flex flex-col justify-center items-center w-screen">
                 {children}
             </main>
