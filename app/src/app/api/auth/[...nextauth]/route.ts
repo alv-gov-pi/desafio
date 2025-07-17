c6b56c7ca573f0d68c4ceeffc079cf52989ab317
Alvaro Alves <alvaro.oliveira@seplan.pi.gov.br>
2025-07-17 13:43:28 -0300

    adição de callbacks

--- app/src/app/api/auth/[...nextauth]/route.ts
+++ app/src/app/api/auth/[...nextauth]/route.ts
@@ -12,29 +12,59 @@ const authOptions: NextAuthOptions = {
       async authorize(credentials, req) {
         const formData = new FormData();
         formData.append('email', `${credentials?.email}`);
-        formData.append('password',  `${credentials?.password}`);
+        formData.append('password', `${credentials?.password}`);
         const response = await fetch('http://localhost:8000/signin', {
-					method: 'POST',
-					body: formData
-				})
-        
-        const respJson = await response.json()
-        const user = { id: '1', name: respJson.usuario.nome, email: respJson.usuario.email, genero: respJson.usuario.genero} 
-				if (user && response.ok) {
-          return user
-				}
+          method: 'POST',
+          body: formData
+        })
 
-				return null
+        const respJson = await response.json()
+        const user = {
+          id: respJson.usuario.id,
+          name: respJson.usuario.nome,
+          email: respJson.usuario.email,
+          genero: respJson.usuario.genero,
+          refresh: respJson.refresh,
+          token: respJson.token,
+        }
+        if (user && response.ok) {
+          return user
+        }
+
+        return null
       }
     })
-  ], 
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
   pages: {
     signIn: "/"
+  },
+  callbacks: {
+    jwt({ token, user }) {
+      if (user) {
+        token.id = user.id
+        token.nome = user.nome
+        token.email = user.email
+        token.accessToken = user.token
+        token.refreshToken = user.refresh
+      }
+      return token
+    },
+    session({ session, token }) {
+      if (token && session.user) {
+        session.user.id = token.id
+        session.user.nome = token.nome
+        session.user.email = token.email
+        session.user.accessToken = token.accessToken
+        session.user.refreshToken = token.refreshToken
+      }
+      return session
+    }
   }
 }
-const handler =  NextAuth(authOptions)
+const handler = NextAuth(authOptions)
 
-export {handler as GET, handler as POST, authOptions}
+export { handler as GET, handler as POST, authOptions }
 
 
 
