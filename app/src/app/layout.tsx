f118d57082ded43484f4bfba14fc5c2bbe3e6f2f
Alvaro Alves <alvaro.oliveira@seplan.pi.gov.br>
2025-07-22 13:13:03 -0300

    remoção de importações não usada

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ColorSchemeScript, MantineProvider, mantineHtmlProps } from '@mantine/core';
 import NextAuthSessionProvider from "@/providers/session-provider";
