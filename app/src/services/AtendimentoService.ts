3ec1ff88937642e4c89b07643b85fd5b6389bb0c
Alvaro Alves <alvaro.oliveira@seplan.pi.gov.br>
2025-07-23 10:34:11 -0300

    add métodos obterSolicitacoesPorSolicitanteId e adicionarAvaliacaoAtendimento

--- app/src/services/AtendimentoService.ts
+++ app/src/services/AtendimentoService.ts
@@ -1,10 +1,19 @@
 import { Atendimento } from "@/types/atendimento";
 import { BaseService } from "./BaseService";
 import { InteracaoAtendimento } from "@/types/interacao-atendimento";
+import { AvaliacaoAtendimento } from "@/types/avaliacao-atendimento";
 
 export class AtendimentoService extends BaseService {
+
     protected dominio: string = 'atendimento';
 
+    async obterSolicitacoesPorSolicitanteId(solicitanteId: number): Promise<Atendimento[]> {
+        const response = await fetch(`http://localhost:8000/solicitacoes/por-solicitante/${solicitanteId}`, {
+            method: "GET"
+        })
+        const solicitacoes: Atendimento[] = await response.json()
+        return solicitacoes
+    }
     async obterAtendimentoPorId(idAtendimento: string): Promise<Atendimento> {
         const response = await fetch(`${this.obterUrlDominio()}/${idAtendimento}/`, {
             method: 'GET',
@@ -22,14 +31,14 @@ export class AtendimentoService extends BaseService {
     }
     async finalizarAtendimento(atendimento: Atendimento) {
         const data = JSON.stringify({
-                "id": atendimento.id,
-                "responsavel": atendimento.responsavel,
-                "servico": atendimento.servico,
-                "solicitante": atendimento.solicitante,
-                "solucao": atendimento.solucao,
-                "atendido": true,
-                "resolvido_em": new Date()
-            })
+            "id": atendimento.id,
+            "responsavel": atendimento.responsavel,
+            "servico": atendimento.servico,
+            "solicitante": atendimento.solicitante,
+            "solucao": atendimento.solucao,
+            "atendido": true,
+            "resolvido_em": new Date()
+        })
         console.log(data)
         const response = await fetch(`${this.obterUrlDominio()}/${atendimento.id}/`, {
             method: 'PUT',
@@ -39,13 +48,32 @@ export class AtendimentoService extends BaseService {
             body: data,
         });
     }
+
+    async adicionarAvaliacaoAtendimento(avaliacaoAtendimento: AvaliacaoAtendimento, idAtendimento: number) {
+        console.log(avaliacaoAtendimento)
+        const response = await fetch(`${this.obterUrlDominio()}/${idAtendimento}/avaliacao/`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(avaliacaoAtendimento)
+        });
+
+        if (!response.ok) {
+            throw new Error(`Erro ao enviar avaliacao de atendimento ${response}`);
+        }
+
+        const novaAvaliacaoAtendimento: AvaliacaoAtendimento = await response.json();
+        return novaAvaliacaoAtendimento;
+    }
+
     async adicionarInterecaoAtendimento(interacao: InteracaoAtendimento): Promise<InteracaoAtendimento> {
         const response = await fetch(`${this.obterUrlDominio()}/${interacao.idAtendimento}/interacao/adicionar/`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({'atendimento' : interacao.idAtendimento, 'texto': interacao.texto, 'tipoUsuario': interacao.tipoUsuario})
+            body: JSON.stringify({ 'atendimento': interacao.idAtendimento, 'texto': interacao.texto, 'tipoUsuario': interacao.tipoUsuario })
         });
 
         if (!response.ok) {
@@ -94,7 +122,7 @@ export class AtendimentoService extends BaseService {
         return data;
     }
 
-    async obterAtentimentoPorServicos(servicos_string: String): Promise<Atendimento[] > {
+    async obterAtentimentoPorServicos(servicos_string: String): Promise<Atendimento[]> {
         try {
             console.log(`${this.obterUrlDominio()}/por-setor/?ids=${servicos_string}/`)
             const response = await fetch(`http://localhost:8000/atendimentos/por-setor/?ids=9`, {
@@ -104,7 +132,7 @@ export class AtendimentoService extends BaseService {
             const atendimentos: Atendimento[] = await response.json()
             return atendimentos
         } catch (error) {
-             throw new Error(`Erro ao solicitar os dados, detalhes: ${error}`);
+            throw new Error(`Erro ao solicitar os dados, detalhes: ${error}`);
         }
     }
 }
