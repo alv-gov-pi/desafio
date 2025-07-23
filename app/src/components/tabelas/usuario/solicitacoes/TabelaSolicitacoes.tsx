d45bd5dbcefe3036f266c58ffe931d5b12fbf183
Alvaro Alves <alvaro.oliveira@seplan.pi.gov.br>
2025-07-23 10:40:38 -0300

    refatoracao uso de componentes AcoesAtendimento e FormAvaliacaoAtendimento

--- app/src/components/tabelas/usuario/solicitacoes/TabelaSolicitacoes.tsx
+++ app/src/components/tabelas/usuario/solicitacoes/TabelaSolicitacoes.tsx
@@ -1,8 +1,10 @@
 import { Button, Group, Table, TableTr, TableTd, TableThead, TableTh, TableTbody } from '@mantine/core';
-import { IconEdit } from '@tabler/icons-react';
 import BotaoExcluirSolicitacao from "@/components/buttons/excluir-solicitacao";
 import { Atendimento } from "@/types/atendimento";
 import { formataStringDate } from '@/utils/databr';
+import BotaoEditarSolicitacao from '@/components/buttons/editar-solicitacao';
+import AcoesAtendimento from '@/components/buttons/acoes-solicitacao';
+import { FormAvaliacaoAtendimento } from '@/components/forms/atendimento/form-avaliacao-atendimento';
 
 export default function TabelaSolicitacoes({ solicitacoes }: { solicitacoes: Atendimento[] }) {
     const linhas = solicitacoes.map((atendimento) => (
@@ -13,26 +15,29 @@ export default function TabelaSolicitacoes({ solicitacoes }: { solicitacoes: Ate
             <TableTd>{atendimento?.responsavel_detalhado?.nome ?? 'Ainda não atribuído'}</TableTd>
             <TableTd>{atendimento.atendido ? 'Sim' : 'Não'}</TableTd>
             <TableTd>
-                <Group justify="center">
-                    <BotaoExcluirSolicitacao id={atendimento.id} />
-                    <Button variant="default" leftSection={<IconEdit size={16} stroke={1.5} />}>Editar</Button>
-                </Group>
+                {!atendimento.atendido &&
+                <AcoesAtendimento atendimento={atendimento}/>}
+                {atendimento.atendido &&
+                <FormAvaliacaoAtendimento atendimento={atendimento}/>}
             </TableTd>
         </TableTr>
     ));
     return (
-        <Table withColumnBorders withTableBorder highlightOnHover striped>
-            <TableThead>
-                <TableTr>
-                    <TableTh>ID</TableTh>
-                    <TableTh>Cadastrado Em</TableTh>
-                    <TableTh>Serviço</TableTh>
-                    <TableTh>Responsavel</TableTh>
-                    <TableTh>Atendido</TableTh>
-                    <TableTh>Ação</TableTh>
-                </TableTr>
-            </TableThead>
-            <TableTbody>{linhas}</TableTbody>
-        </Table>
+        <div className="space-y-4 rounded-md bg-white p-6 shadow-md border border-gray-200 mt-4 w-9/12">
+            <h1 className="text-xl font-semibold text-content-emphasis">Suas solicitações</h1>
+            <Table withColumnBorders withTableBorder highlightOnHover striped>
+                <TableThead>
+                    <TableTr>
+                        <TableTh>ID</TableTh>
+                        <TableTh>Cadastrado Em</TableTh>
+                        <TableTh>Serviço</TableTh>
+                        <TableTh>Responsavel</TableTh>
+                        <TableTh>Atendido</TableTh>
+                        <TableTh>Ação</TableTh>
+                    </TableTr>
+                </TableThead>
+                <TableTbody>{linhas}</TableTbody>
+            </Table>
+        </div>
     )
 }
