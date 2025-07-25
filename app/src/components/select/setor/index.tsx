53a5d7aedd56444b6b548a5187e6ceec3221d6e5
Alvaro Alves <alvaro.oliveira@seplan.pi.gov.br>
2025-07-25 12:06:47 -0300

    alteracao para use cliente

--- app/src/components/select/setor/index.tsx
+++ app/src/components/select/setor/index.tsx
@@ -1,14 +1,23 @@
+'use client'
+import { SetorService } from "@/services/SetorService"
 import { Setor } from "@/types/setor"
+import { useEffect, useState } from "react";
 
-export default async function SelectSetor({ id_setor_atual }: {id_setor_atual?: string}) {
-    
-    const setores: Setor[] = await fetch(`http://localhost:8000/setores.json`)
-        .then(res => res.json())
-  
+export default function SelectSetor({ id_setor_atual, onChange  }: {id_setor_atual?: string, onChange: (event: React.ChangeEvent<HTMLSelectElement>) => void}) {
+    const setorService: SetorService = new SetorService();
+    const [setores, setSetores ] = useState<Setor[]>([]);
+    useEffect(() => {
+         const buscarSetores = async () => {
+            const setoresRecuperados: Setor[] = await setorService.obterTodosSetores();
+            setSetores(setoresRecuperados);
+         }
+         buscarSetores()
+    }, [setores])
+   
     return (
-        <select name="setor" id="setor" className="border border-sky-600 rounded-sm" defaultValue={id_setor_atual}>
+        <select name="setor" id="setor" className="border border-sky-600 rounded-sm" defaultValue={id_setor_atual} onChange={onChange}>
             {setores.map((setor: Setor) => (
-                <option key={setor.id.toString()} value={setor.id.toString()}>{setor.nome}</option>
+                <option key={setor.id.toString()} value={setor.id}>{setor.nome}</option>
             ))}
         </select>
     )
