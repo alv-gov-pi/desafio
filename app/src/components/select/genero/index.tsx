a2d684cefdfcac63d650ef4a8b2ed41635a90b63
Alvaro Alves <alvaro.oliveira@seplan.pi.gov.br>
2025-07-25 11:57:23 -0300

    alteracao para use cliente

--- app/src/components/select/genero/index.tsx
+++ app/src/components/select/genero/index.tsx
@@ -1,7 +1,7 @@
-
-export default function SelectGenero({ sigla }: {sigla: string}) {
+'use client'
+export default function SelectGenero({ sigla, onChange }: {sigla: string, onChange: (event: React.ChangeEvent<HTMLSelectElement>) => void}) {
     return (
-        <select name="genero" id="genero" className="border border-sky-600 rounded-sm" defaultValue={sigla}>
+        <select name="genero" id="genero" className="border border-sky-600 rounded-sm" defaultValue={sigla} onChange={onChange}>
             <option value="F" >Feminino</option>
             <option value="M" >Masculino</option>
         </select>
