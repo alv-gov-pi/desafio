2277080698ecf35c7785384955d55d326e13b4b7
Alvaro Alves <alvaro.oliveira@seplan.pi.gov.br>
2025-07-25 11:55:49 -0300

    substituição  código local por classe service

--- app/src/app/usuario/[usuarioId]/page.tsx
+++ app/src/app/usuario/[usuarioId]/page.tsx
@@ -1,15 +1,14 @@
 import FormEditarUsuario from "@/components/forms/usuario/editar";
 import TemplateApp from "@/components/template/autenticado/template";
+import { UsuarioService } from "@/services/UsuarioService";
 import { Usuario } from "@/types/usuario";
 import React from "react";
 
 export default async function pageEditar({ params }: { params: Promise<{ usuarioId: number }> }) {
     const { usuarioId } = await params;
-    const response = await fetch(`http://localhost:8000/usuario/${usuarioId}`, {
-        method: "GET"
-    })
-    
-    const usuario: Usuario = await response.json();
+    const usuarioService = new UsuarioService();
+
+    const usuario: Usuario = await usuarioService.obterUsuarioPorId(usuarioId)
 
     return (
         <TemplateApp>
