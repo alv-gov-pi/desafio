57c1d3ef42edcbab1ca8f0beda357202cff81cc8
Alvaro Alves <alvaro.oliveira@seplan.pi.gov.br>
2025-07-25 12:06:19 -0300

    componentes do painel

---
+++ app/src/components/charts/atendimento/big-number.tsx
@@ -0,0 +1,14 @@
+'use client'
+import { Paper } from '@mantine/core';
+type props = { titulo: string, 
+    numero: number, 
+    cor: string 
+}
+export default function BigNumber({ titulo, numero, cor }: props) {
+    return (
+        <Paper shadow="xs" p="xl" className='flex flex-col justify-between' withBorder >
+            <div className='flex justify-center text-2xl'>{titulo}</div>
+            <div className={`flex justify-center ${cor}   text-8xl`}>{numero}</div>
+        </Paper >
+    );
+}
