8f416531e70ed1d2c70765b106614dc1b223119e
Alvaro Alves de Oliveira <alvaro.oliveira@seplan.pi.gov.br>
2025-07-27 11:51:29 -0300

    customizing next-auth default types

--- app/src/types/next-auth.d.ts
+++ app/src/types/next-auth.d.ts
@@ -1,11 +1,35 @@
 import NextAuth from 'next-auth'
 
 declare module 'next-auth' {
-    interface Session {
-        usuario: {
-            id: string,
-            email: string,
-            nome: string
+     interface Session {
+        user: {
+            id: string | undefined,
+            nome: string | undefined | null,
+            email: string | undefined | null,
+            genero: string | undefined,
+            refresh: string | undefined,
+            access: string | undefined,
+            setor: number | undefined,
         }
+    }
+    interface User {
+        nome: string | undefined | null,
+        id: string,
+        genero: string,
+        refresh: string,
+        access: string,
+        setor: number,
+    }
+}
+
+declare module "next-auth/jwt" {
+    interface JWT {
+        id: string | undefined,
+        nome: string | undefined | null,
+        email: string | undefined | null,
+        genero: string | undefined,
+        refresh: string | undefined,
+        access: string | undefined,
+        setor: number | undefined,
     }
 }
