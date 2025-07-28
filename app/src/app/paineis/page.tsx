15d295fce642457f9b691bf39fdc915a503fec6b
Alvaro Alves <alvaro.oliveira@seplan.pi.gov.br>
2025-07-28 08:34:44 -0300

    remove fake data

--- app/src/app/paineis/page.tsx
+++ app/src/app/paineis/page.tsx
@@ -6,49 +6,34 @@ import TabelaQuantidadeAtendimentoPorResponsavel from "@/components/tabelas/usua
 import TemplateApp from "@/components/template/autenticado/template"
 import { AtendimentoService } from "@/services/AtendimentoService";
 import { AtendimentoPorData } from "@/types/atendimento-por-data";
+import { AtendimentoTotais } from "@/types/atendimentos-totais";
 import { AvaliacaoAtendimentoPorServico } from "@/types/avaliacao-atendimento-por-servico";
 import { AvaliacaoAtendimentoPorSetor } from "@/types/avaliacao-atendimento-por-setor";
 import { QuantidadeAtendimentoPorResponsavel } from "@/types/quantidade-atendimento-por-responsavel";
+import { formataParaTresDigitos } from "@/utils/formataNumero"; 
 
 export default async function paineis({ params }: { params: Promise<{ id: number }> }) {
     const { id } = await params;
     const atendimentoService = new AtendimentoService();
     const atendimentoPorResponsavel: QuantidadeAtendimentoPorResponsavel[] = await atendimentoService.obterQuantidadedeAtendimentosPorResponsavel();
     console.log(atendimentoPorResponsavel);
-    const media_nota_por_setor: AvaliacaoAtendimentoPorSetor[] = [
-        {
-            setor_nome: "Secretaria de Planejamento do Piaui",
-            media_nota: 5.0
-        }
-    ]
-    const media_nota_por_servico: AvaliacaoAtendimentoPorServico[] = [
-        {
-            servico_nome: "Manutenção de site",
-            media_nota: 5.0
-        }
-    ]
+    const media_nota_por_setor: AvaliacaoAtendimentoPorSetor[] = await atendimentoService.obterEstatisticasAvaliacaoPorSetorSolicitante()
+    const media_nota_por_servico: AvaliacaoAtendimentoPorServico[] = await atendimentoService.obterEstatisticasAvaliacaoPorServicoSolicitado()
 
-    const agrupamento_uniao_datas: AtendimentoPorData[] = [
-            {
-                data: "2025-07-17",
-                atendidos: 1,
-                nao_atendidos: 1
-            },
-            {
-                data: "2025-07-22",
-                atendidos: 1,
-                nao_atendidos: 0
-            }
-        ]
+    const agrupamento_uniao_datas: AtendimentoPorData[] = await atendimentoService.obterEstatisticasAtentimentoPorData();
+    const atendimentoTotais: AtendimentoTotais = await atendimentoService.obterEstatisticasTotais();
+    const totais: string = formataParaTresDigitos(atendimentoTotais.nao_atendidos + atendimentoTotais.atendidos);
+    const abertos: string = formataParaTresDigitos(atendimentoTotais.nao_atendidos);
+    const  fechados: string = formataParaTresDigitos(atendimentoTotais.atendidos);
     return (
         <TemplateApp >
             <div className="flex w-8/12 justify-between gap-2 space-y-4 rounded-md bg-white p-6 shadow-md border border-gray-200">
                 <div className="flex flex-col h-12/12   w-8/12 justify-between  space-y-4 rounded-md bg-white p-6 shadow-md border border-gray-200 ">
                     <h1 className="text-xl font-semibold text-content-emphasis">Estátisticas dos chamados</h1>
                     <div className="flex justify-between">
-                        <BigNumber titulo="Total" numero={200} cor="text-sky-600" ></BigNumber>
-                        <BigNumber titulo="Abertos" numero={100} cor="text-red-600" ></BigNumber>
-                        <BigNumber titulo="Fechados" numero={100} cor="text-teal-600" ></BigNumber>
+                        <BigNumber titulo="Total" numero={totais} cor="text-sky-600" ></BigNumber>
+                        <BigNumber titulo="Abertos" numero={abertos} cor="text-red-600" ></BigNumber>
+                        <BigNumber titulo="Fechados" numero={fechados} cor="text-teal-600" ></BigNumber>
                     </div>
                     <div className="flex flex-col ">
                         <LineChartAberturaFechamento atendimentosPorData={agrupamento_uniao_datas}/>
