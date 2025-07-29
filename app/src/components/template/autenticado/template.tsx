4e57cfdcb242d3ee9abc662f8dae9d2be404e785
Alvaro Alves <alvaro.oliveira@seplan.pi.gov.br>
2025-07-29 12:00:14 -0300

    adição componente Notifications

--- app/src/components/template/autenticado/template.tsx
+++ app/src/components/template/autenticado/template.tsx
@@ -2,10 +2,13 @@ import HeaderApp from "./header/header";
 import FooterApp from "./footer";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { getServerSession } from "next-auth";
+import { Notifications } from '@mantine/notifications';
 export  default async function TemplateApp({ children }: { children: React.ReactNode }) {
     const  session = await getServerSession(authOptions)
     return (
+
         <div className="flex flex-col h-screen justify-between">
+            <Notifications />
             <HeaderApp usuario_id={session?.user.id} />
             <main className="flex flex-col justify-center items-center w-screen">
                 {children}
