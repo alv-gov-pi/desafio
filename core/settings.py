f3363276d45ddca1c6de186853822b514a6b91b7
Alvaro Alves <alvaro.oliveira@seplan.pi.gov.br>
2025-07-29 11:46:20 -0300

    alteração para solicitar antenticação por padrão

--- core/settings.py
+++ core/settings.py
@@ -37,7 +37,10 @@ ALLOWED_HOSTS = []
 REST_FRAMEWORK = {
      'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated', # Requer autenticação por padrão
+    ),
 }
 
 SIMPLE_JWT = {
