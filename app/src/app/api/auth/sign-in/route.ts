6f57438265670f5f7e2bc6e3c9a4569a3a87b2ac
Alvaro Alves <alvaro.oliveira@seplan.pi.gov.br>
2025-07-29 12:20:04 -0300

    remoção de arquivos e pastas não usados

--- app/src/app/api/auth/sign-in/route.ts
+++
@@ -1,7 +0,0 @@
-import { signIn } from "@/auth";
-
-export async function GET(request: Request) {
-    return signIn('CredentialsProvedor', {
-        redirectTo: 'http://localhost:3000/login'
-    })
-}
