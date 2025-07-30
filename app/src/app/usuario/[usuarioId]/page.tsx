77a2da2ac88dff7f488c927c6859923218201f1e
Alvaro Alves <alvaro.oliveira@seplan.pi.gov.br>
2025-07-30 11:06:05 -0300

    refactor becase change in FormEditarUsuario

--- app/src/app/usuario/[usuarioId]/page.tsx
+++ app/src/app/usuario/[usuarioId]/page.tsx
@@ -1,7 +1,9 @@
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import FormEditarUsuario from "@/components/forms/usuario/editar";
 import TemplateApp from "@/components/template/autenticado/template";
+import { SetorService } from "@/services/SetorService";
 import { UsuarioService } from "@/services/UsuarioService";
+import { Setor } from "@/types/setor";
 import { Usuario } from "@/types/usuario";
 import { getServerSession } from "next-auth";
 import React from "react";
@@ -11,13 +13,14 @@ export default async function pageEditar({ params }: { params: Promise<{ usuario
     const session = await getServerSession(authOptions)
     const token: string  = session?.user.access;
     const usuarioService = new UsuarioService(token);
-
+    const setorService: SetorService = new SetorService(token);
+    const setores: Setor[] = await setorService.obterTodosSetores();
     const usuario: Usuario = await usuarioService.obterUsuarioPorId(usuarioId)
 
 
     return (
         <TemplateApp>
-            <FormEditarUsuario usuario={usuario} token={token} />
+            <FormEditarUsuario usuario={usuario} token={token} setores={setores}/>
         </TemplateApp>
     )
 }
--- app/src/app/usuario/cadastro/page.tsx
+++ app/src/app/usuario/cadastro/page.tsx
@@ -1,10 +1,16 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import FormCadastroUsuario from "@/components/forms/usuario/cadastro";
 import TemplateNaoAtenticado from "@/components/template/nao-autenticado/template";
-export default function PaginaCadastro() {
+import { SetorService } from "@/services/SetorService";
+import { getServerSession } from "next-auth";
+export default async  function PaginaCadastro() {
+    const session = await getServerSession(authOptions)
+    const token: string  = session?.user.access;
+    const setorService: SetorService = new SetorService(token);
+    const setores = await setorService.obterTodosSetores();
     return (
         <TemplateNaoAtenticado>
-            <FormCadastroUsuario />
+            <FormCadastroUsuario setores={setores}/>
         </TemplateNaoAtenticado>
-
     );
 }
