6312822a5d567e48612e9ece207cc59d96762d37
Alvaro Alves <alvaro.oliveira@seplan.pi.gov.br>
2025-07-30 11:03:46 -0300

    remove local code to service

--- app/src/app/usuario/listar/page.tsx
+++ app/src/app/usuario/listar/page.tsx
@@ -1,17 +1,21 @@
-'use client'
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import TabelaUsuario from "@/components/tabelas/usuario";
-import { useState, useEffect } from "react";
-export default function listarUsuario() {
-    const [posts, setUsuarios] = useState([])
-    useEffect(() => {
-        fetch('localhost:8000/usuarios')
-        .then(res => res.json())
-        .then(data => setUsuarios(data.usuarios))
-    }, [])
+import TemplateApp from "@/components/template/autenticado/template";
+import { UsuarioService } from "@/services/UsuarioService";
+import { Usuario } from "@/types/usuario";
+import { getServerSession } from "next-auth";
+
+export default async function listarUsuario() {
+    const session = await getServerSession(authOptions)
+    const token: string = session?.user.access;
+    const usuarioService: UsuarioService = new UsuarioService(token);
+    const usuarios: Usuario[] = await usuarioService.obterUsuarios();
+
     return(
-        <main>
+        <TemplateApp>
             <h1>Todos os Usuários</h1>
-            <TabelaUsuario/>
-        </main>
+            <TabelaUsuario usuarios={usuarios}/>
+        </TemplateApp>
+       
     )
 }
