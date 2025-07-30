b9057c417b1a7461cda2ca2c363f8945ac897225
Alvaro Alves <alvaro.oliveira@seplan.pi.gov.br>
2025-07-30 12:45:21 -0300

    use next Image instead img

--- app/src/components/backgrounds/index.tsx
+++ app/src/components/backgrounds/index.tsx
@@ -1,6 +1,13 @@
+import Image from 'next/image'
 function BackgroundLadoEsquerdo() {
   return (
-    <div className='mr-10 w-8/12'><img className="h-screen" src="login-left-bg.png" alt="" /></div>
+    <div className='mr-10 w-8/12'>
+      <Image src="/image/login.png"
+        alt="imagem de uma pessoa usando notebook"
+        width={200} height={800}
+        className="h-screen w-3/6"
+      />
+    </div>
   );
 }
 
