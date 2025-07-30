9cfa94f0046a8ce2d6882bc362533cd4aad13461
Alvaro Alves <alvaro.oliveira@seplan.pi.gov.br>
2025-07-30 10:59:26 -0300

    add accessTokenExpires

--- api/signin.py
+++ api/signin.py
@@ -5,6 +5,8 @@ from rest_framework.exceptions import APIException
 from api.serializers import UsuarioSerializer
 from api.auth import Autenticacao
 from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.settings import api_settings
+from datetime import datetime, timedelta
 
 class Signin(APIView):
     permission_classes=[AllowAny]
@@ -22,5 +24,6 @@ class Signin(APIView):
         return Response({
             "usuario": serializer.data,
             "refresh": str(token),
-            "access": str(token.access_token)
+            "access": str(token.access_token),
+            "accessTokenExpires": datetime.now() + timedelta(minutes=5)
         })
