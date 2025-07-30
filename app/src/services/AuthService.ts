740985c3aae85b6a5c6d630ac7db7bf22000f913
Alvaro Alves <alvaro.oliveira@seplan.pi.gov.br>
2025-07-30 10:59:41 -0300

    add meth refreshToken

--- app/src/services/AuthService.ts
+++ app/src/services/AuthService.ts
@@ -1,18 +1,28 @@
 import HTTPMethod from "http-method-enum";
 
 export class AuthService {
-    protected baseUrl = 'http://localhost:8000';
-    async login(email: string, password: string) {
-        const data = JSON.stringify({email: email,  password: password})
-        const response = await fetch(`${this.baseUrl}/signin`, {
-          method: HTTPMethod.POST,
-          headers: {
-                'Content-Type': 'application/json',
-            },
-          body: data
-        })
-        return response;
-    }
+  protected baseUrl = 'http://localhost:8000';
+  async login(email: string, password: string) {
+    const data = JSON.stringify({ email: email, password: password })
+    const response = await fetch(`${this.baseUrl}/signin`, {
+      method: HTTPMethod.POST,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: data
+    })
+    return response;
+  }
 
-
+  async refreshToken(refresh: string) {
+    const data = JSON.stringify({ refresh })
+    const response = await fetch(`${this.baseUrl}/api/token/refresh/`, {
+      method: HTTPMethod.POST,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: data
+    })
+    return response;
+  }
 }
