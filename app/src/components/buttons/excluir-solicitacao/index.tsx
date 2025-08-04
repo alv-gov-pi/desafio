533dfbdd8d6df44bbaecc614723b549e91c64562
Alvaro Alves <alvaro.oliveira@seplan.pi.gov.br>
2025-08-04 12:33:41 -0300

    move código de notificação para componente externo

--- app/src/components/buttons/excluir-solicitacao/index.tsx
+++ app/src/components/buttons/excluir-solicitacao/index.tsx
@@ -1,26 +1,28 @@
 'use client'
 import { IconTrash } from '@tabler/icons-react';
-import { Button, Tooltip, Notification } from '@mantine/core';
+import { Button, Tooltip } from '@mantine/core';
 import { AtendimentoService } from '@/services/AtendimentoService';
 import { SyntheticEvent } from 'react';
-import { notifications } from '@mantine/notifications';
 import { useModals } from '@mantine/modals';
-export default function BotaoExcluirSolicitacao({ id, token }: { id: number, token: string }) {
+import mostrarNotificacao from '@/utils/notification';
+
+import { useRouter } from 'next/navigation';
+
+export default function BotaoExcluirSolicitacao({ id, token, solicitanteId }: { id: number, token: string, solicitanteId: number }) {
     const atendimentoService: AtendimentoService = new AtendimentoService(token);
     const modals = useModals();
+    const router = useRouter();
     function notificar() {
-        notifications.show({
-            color: 'red',
-            title: 'Você deletou a solicitação',
-            message: 'Você deletou a solicitação, recarregue a página !!!',
-            autoClose: 5000,
-        });
+        mostrarNotificacao('Você deletou a solicitação', 'Você deletou a solicitação!!!', 'red')
+        router.refresh();  
     }
+
     function excluirDefinitivamente() {
         console.log(id);
         notificar();
         atendimentoService.exluirAtendimento(id);
     }
+    
     function confirmarExclusao(event: SyntheticEvent) {
         event.preventDefault();
 
@@ -32,6 +34,7 @@ export default function BotaoExcluirSolicitacao({ id, token }: { id: number, tok
             onConfirm: () => excluirDefinitivamente(),
         });
     }
+
     return (
         <Tooltip label="Excluir solicitação">
             <Button component="a" onClick={e => confirmarExclusao(e)} variant="filled" color="red" leftSection={<IconTrash size={16} stroke={1.5} />} >Excluir</Button>
