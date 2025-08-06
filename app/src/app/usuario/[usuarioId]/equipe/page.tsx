5d557798cbad4790434952f098a21cbdba3bccf4
Alvaro Alves <alvaro.oliveira@seplan.pi.gov.br>
2025-08-06 11:23:37 -0300

    add carpadrao

--- app/src/app/usuario/[usuarioId]/equipe/page.tsx
+++ app/src/app/usuario/[usuarioId]/equipe/page.tsx
@@ -4,6 +4,8 @@ import TemplateApp from "@/components/template/autenticado/template";
 import { UsuarioService } from "@/services/UsuarioService";
 import { Usuario } from "@/types/usuario";
 import { getServerSession } from "next-auth";
+import FiltroUsuario from "@/components/forms/usuario/filtro";
+import CardPadrao from "@/components/card";
 
 export default async function listarUsuario() {
     const session = await getServerSession(authOptions)
@@ -14,10 +16,10 @@ export default async function listarUsuario() {
 
     return (
         <TemplateApp>
-            <div className="space-y-4 rounded-md bg-white p-6 shadow-md border border-gray-200 mt-4 w-10/12 flex flex-col">
-                <h1 className="text-xl font-semibold text-content-emphasis">Membros da equipe </h1>
-                <TabelaUsuario usuarios={usuarios} />
-            </div>
+            <FiltroUsuario />
+            <CardPadrao titulo="Membros da Equipe">
+                <TabelaUsuario usuarios={usuarios} token={token} />
+            </CardPadrao>
         </TemplateApp>
 
     )
